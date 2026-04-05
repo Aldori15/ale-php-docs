9bfba7a6fae7760adb823a1dbf42dfe2d1bf5044
mostlynick3 <nick@nikitas.no>
2026-04-05 20:03:30 +0200

    Implement source structure and gitignore

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'headers_dir' => __DIR__ . '/mod-ale/src/LuaEngine/methods',
+    'headers_dir' => __DIR__ . '/source/mod-ale/src/LuaEngine/methods',
     'site_title'  => 'Eluna / ALE API',
 ];
